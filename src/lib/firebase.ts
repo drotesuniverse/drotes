40ef181ed6c665fabc103b3e55a897645d12947a
drotesuniverse <drotesuniverse@gmail.com>
2026-02-07 16:37:17 +0400

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,6 +2,7 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics, isSupported } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDSv2nm_EqLJ_KJOrF4cFyrwGq3pefcpgM",
     authDomain: "otp-drotes.firebaseapp.com",
@@ -11,10 +12,11 @@ const firebaseConfig = {
     appId: "1:934728104379:web:bdfecc5bd350efa870ac9e",
     measurementId: "G-BC50N9Q6B0"
 };
+
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
-const db = getFirestore(app);
+
 let analytics;
 if (typeof window !== "undefined") {
     isSupported().then((supported) => {
@@ -23,4 +25,7 @@ if (typeof window !== "undefined") {
         }
     });
 }
+
+const db = getFirestore(app);
+
 export { app, auth, analytics, db };
