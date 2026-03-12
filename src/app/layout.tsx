3024369a31967af8cada744260d79b3ae19e2a28
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2026-03-12 20:30:29 +0530

    fix: members only page redirect loop issue / removed member guard and integrated the redirect feature with middleware

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,9 +39,9 @@ export default function RootLayout({
                         <CurrencyProvider>
                             <PopupManager />
                             <LiveTracker />
-                            <MembersGuard>
+                            {/* <MembersGuard> */}
                                 {children}
-                            </MembersGuard>
+                            {/* </MembersGuard> */}
                         </CurrencyProvider>
                     </ApolloProvider>
                 </body>
