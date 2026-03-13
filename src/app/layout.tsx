4a833570e990335748d00f726eaff556c92b3d8a
drotesuniverse <drotesuniverse@gmail.com>
2026-03-14 02:05:52 +0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 import { ReactLenis } from "lenis/react";
@@ -9,24 +8,31 @@ import { client } from "@/lib/apollo";
 import PopupManager from "@/components/PopupManager";
 import MembersGuard from "@/components/MembersGuard";
 import LiveTracker from "@/components/LiveTracker";
-import Script from "next/script"; // <-- ADD THIS IMPORT
-
 const geistSans = Geist({
     variable: "--font-geist-sans",
     subsets: ["latin"],
 });
-
 const geistMono = Geist_Mono({
     variable: "--font-geist-mono",
     subsets: ["latin"],
 });
-
 const poppins = Poppins({
     variable: "--font-poppins",
     subsets: ["latin"],
     weight: ["400", "700", "900"],
 });
-
+const META_PIXEL_SCRIPT = `
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1473126554430964');
+fbq('track', 'PageView');
+`;
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -36,6 +42,7 @@ export default function RootLayout({
         <html lang="en" className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased`} suppressHydrationWarning>
             <head>
                 {/* Meta Pixel Code */}
+                <script dangerouslySetInnerHTML={{ __html: META_PIXEL_SCRIPT }} />
                 <noscript>
                     <img height="1" width="1" style={{ display: "none" }}
                         src="https://www.facebook.com/tr?id=1473126554430964&ev=PageView&noscript=1"
@@ -44,22 +51,6 @@ export default function RootLayout({
             </head>
             <ReactLenis root>
                 <body suppressHydrationWarning style={{ zoom: "90%" }}>
-                    {/* Meta Pixel Script */}
-                    <Script id="meta-pixel" strategy="beforeInteractive">
-                        {`
-                            !function(f,b,e,v,n,t,s)
-                            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                            n.queue=[];t=b.createElement(e);t.async=!0;
-                            t.src=v;s=b.getElementsByTagName(e)[0];
-                            s.parentNode.insertBefore(t,s)}(window, document,'script',
-                            'https://connect.facebook.net/en_US/fbevents.js');
-                            fbq('init', '1473126554430964');
-                            fbq('track', 'PageView');
-                        `}
-                    </Script>
-
                     <ApolloProvider client={client}>
                         <CurrencyProvider>
                             <PopupManager />
